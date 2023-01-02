8453fe42c08250d9d255ed171763af44b75d351f
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-01-02 21:04:07 +0330

    Map on feedbacks and show them in FeedbackItem

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,9 +1,13 @@
-const FeedbackList = ({feedback}) => {
+import FeedbackItem from "./FeedbackItem"
 
+const FeedbackList = ({ feedback }) => {
+    console.log(feedback)
+    if (!feedback || feedback.length === 0) {
+        return <p>No feedback yet</p>
+    }
     return (
-        <div>
-           List
-
+        <div className="feedback-list">
+            {feedback.map((item) => <FeedbackItem key={item.id} item={item} />)}
         </div>
     )
 }
