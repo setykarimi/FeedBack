507c6e2a795f5468e2f12c15b075a8a21f003fdd
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-01-02 20:42:25 +0330

    Create new folder for datas, add FeedbackData

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,24 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 10,
+        text: 'Lorem Ipsum dolor sit amet elit, vel vitae commodi alias volupteam est volupteam ipsa quee.'
+    },
+    {
+        id: 2,
+        rating: 5,
+        text: 'Ipsum dolor sit amet elit, vel vitae commodi alias volupteam est volupteam ipsa quee.'
+    },
+    {
+        id: 3,
+        rating: 8,
+        text: 'vel vitae commodi alias volupteam est volupteam ipsa quee.'
+    },
+    {
+        id: 4,
+        rating: 6,
+        text: 'sit amet elit, vel vitae commodi alias volupteam est volupteam ipsa quee.'
+    },
+]
+
+export default FeedbackData
