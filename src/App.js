215a25837a3639998aa106d2d5831c5bb62f3ce1
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-01-02 20:43:01 +0330

    Add FeedbackList compoent to app.js, create new state for feedbackdata

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import { useState } from 'react';
 import './App.css';
-import FeedbackItem from './components/FeedbackItem';
+import FeedbackList from './components/FeedbackList';
 import Header from './components/Header';
+import FeedbackData from './data/FeedbackData';
 
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData)
+
   return (
     <div className="App">
       <Header />
-      <FeedbackItem />
+      <FeedbackList feedback={feedback}/>
     </div>
   );
 }
