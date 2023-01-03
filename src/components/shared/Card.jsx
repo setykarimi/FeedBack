f3a601fc3763583a1c00f823c61516871de023ce
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-01-03 08:20:43 +0330

    Create new folder for shared components, create Card component

---
+++ src/components/shared/Card.jsx
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types';
+
+const Card = ({ children, reverse }) => {
+    return (
+        <div className={`card ${reverse && reverse}`}>
+            {children}
+        </div>
+    )
+}
+
+Card.defaultProps = {
+    reverse: false
+}
+
+Card.prototypes ={
+    reverse: PropTypes.bool,
+    children: PropTypes.node.isRequired
+}
+
+export default Card
