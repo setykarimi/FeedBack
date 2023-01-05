01f813db79bb3390b97420a558c43aa30d756732
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-01-05 15:26:34 +0330

    Add feedback form, edit style

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
+import { v4 as uuidv4 } from 'uuid'
 import { useState } from 'react';
 import './App.css';
 import FeedbackList from './components/FeedbackList';
 import Header from './components/Header';
 import FeedbackData from './data/FeedbackData';
 import FeedbackStats from './components/FeedbackStats';
+import FeedbackForm from './components/FeedbackForm';
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4()
+    setFeedback([...feedback, newFeedback])
+  }
 
   const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
       setFeedback(feedback.filter((item) => item.id !== id))
     }
-
   }
 
   return (
     <>
       <Header />
-      <div className="App">
+      <div className="App text-white w-fit mx-auto p-5 md:w-1/2 xl:w-1/3">
+        <FeedbackForm handleAdd={addFeedback} />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
