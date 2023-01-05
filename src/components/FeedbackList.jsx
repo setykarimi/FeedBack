56f1483a1d4758adca5bbae09acc97ada35b618b
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-01-05 10:33:58 +0330

    Pass DeleteFeedback as prop to feedbackItem

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,14 +1,13 @@
 import PropTypes from 'prop-types';
 import FeedbackItem from "./FeedbackItem";
 
-const FeedbackList = ({ feedback }) => {
-    console.log(feedback)
+const FeedbackList = ({ feedback, handleDelete }) => {
     if (!feedback || feedback.length === 0) {
         return <p>No feedback yet</p>
     }
     return (
         <div className="feedback-list">
-            {feedback.map((item) => <FeedbackItem key={item.id} item={item} />)}
+            {feedback.map((item) => <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>)}
         </div>
     )
 }
