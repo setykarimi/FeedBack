6a5ffd3fedf4abe1f5ed34962a73185bd086dd8c
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-01-05 10:33:30 +0330

    Add deleteFeedback function and pass it to feedbackList as a prop

--- src/App.js
+++ src/App.js
@@ -5,12 +5,19 @@ import Header from './components/Header';
 import FeedbackData from './data/FeedbackData';
 
 function App() {
-  const [feedback, setFeedback] = useState(FeedbackData)
+  const [feedback, setFeedback] = useState(FeedbackData);
+
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      setFeedback(feedback.filter((item) => item.id !== id))
+    }
+
+  }
 
   return (
     <div className="App">
       <Header />
-      <FeedbackList feedback={feedback}/>
+      <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
     </div>
   );
 }
