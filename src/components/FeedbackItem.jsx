7be12527015a11225fdfd4b6ea30ea0b0d006512
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-01-05 10:34:42 +0330

    Add deleteButton and add fontAwesome icon to it, set prop for deleteHandler function

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,20 +1,27 @@
+import { FaTimes } from 'react-icons/fa'
 import PropTypes from 'prop-types';
 import Card from "./shared/Card";
 
-const FeedbackItem = ({item}) => {
+const FeedbackItem = ({ item, handleDelete }) => {
+
+    
+
     return (
         <Card reverse={true}>
             <div className="num-display">
                 {item.rating}
             </div>
+            <button onClick={() => handleDelete(item.id)} className='close'>
+                <FaTimes color='purple' />
+            </button>
             <div className="text-display">
-               {item.text}
+                {item.text}
             </div>
         </Card>
     )
 }
 
-FeedbackItem.propTypes={
+FeedbackItem.propTypes = {
     item: PropTypes.object.isRequired
 }
 
