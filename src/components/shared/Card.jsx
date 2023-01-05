4b661e32e12f8c458e39057619b975ebee8cdd96
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-01-05 15:30:29 +0330

    Edit className

--- src/components/shared/Card.jsx
+++ src/components/shared/Card.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 const Card = ({ children, reverse }) => {
     return (
-        <div className={`card ${reverse && reverse}`}>
+        <div className={`bg-white text-zinc-900 p-4 relative my-4 rounded-lg ${reverse && 'bg-zinc-800'}`}>
             {children}
         </div>
     )
