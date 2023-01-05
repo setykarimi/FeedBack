7a6995cf474c51367e1d8e9417a6d70b447a8890
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-01-05 15:29:40 +0330

    Defing new variable for inputClassName, edit style

--- src/components/RatingSelect.jsx
+++ src/components/RatingSelect.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react"
 
-const RatingSelect = ({select}) => {
+const RatingSelect = ({ select }) => {
     const [selected, setSelected] = useState(10);
+    const inputClassName = 'form-check-input appearance-none rounded-full h-10 w-10 border border-gray-300 bg-white checked:bg-orange-400 checked:border-orange-500 focus:outline-none transition duration-200 my-1 align-top bg-no-repeat bg-center bg-contain float-left  cursor-pointer'
 
     const handleChange = (e) => {
         setSelected(+ e.currentTarget.value)
@@ -9,106 +10,116 @@ const RatingSelect = ({select}) => {
     }
 
     return (
-        <ul className="rating">
-            <li>
-                <input 
-                type="radio"
-                id="num1"
-                name="rating"
-                value='1'
-                onChange={handleChange}
-                checked={selected === 1} />
-                <label htmlFor="num1">1</label>
+        <ul className="rating flex flex-wrap gap-2 justify-center items-center">
+            <li className="relative flex items-center justify-center">
+                <input
+                    className={inputClassName}
+                    type="radio"
+                    id="num1"
+                    name="rating"
+                    value='1'
+                    onChange={handleChange}
+                    checked={selected === 1} />
+                <label className="absolute mx-auto font-bold"htmlFor="num1">1</label>
             </li>
-            <li>
-                <input 
-                type="radio"
-                id="num2"
-                name="rating"
-                value='2'
-                onChange={handleChange}
-                checked={selected === 2} />
-                <label htmlFor="num2">2</label>
+            <li className="relative flex items-center justify-center">
+                <input
+                    className={inputClassName}
+                    type="radio"
+                    id="num2"
+                    name="rating"
+                    value='2'
+                    onChange={handleChange}
+                    checked={selected === 2} />
+                <label className="absolute mx-auto font-bold"htmlFor="num2">2</label>
             </li>
-            <li>
-                <input 
-                type="radio"
-                id="num3"
-                name="rating"
-                value='3'
-                onChange={handleChange}
-                checked={selected === 3} />
-                <label htmlFor="num3">3</label>
+            <li className="relative flex items-center justify-center">
+                <input
+                className={inputClassName}
+                    type="radio"
+                    id="num3"
+                    name="rating"
+                    value='3'
+                    onChange={handleChange}
+                    checked={selected === 3} />
+                <label className="absolute mx-auto font-bold"htmlFor="num3">3</label>
             </li>
-            <li>
-                <input 
-                type="radio"
-                id="num4"
-                name="rating"
-                value='4'
-                onChange={handleChange}
-                checked={selected === 4} />
-                <label htmlFor="num4">4</label>
+            <li className="relative flex items-center justify-center">
+                <input
+                className={inputClassName}
+                    type="radio"
+                    id="num4"
+                    name="rating"
+                    value='4'
+                    onChange={handleChange}
+                    checked={selected === 4} />
+                <label className="absolute mx-auto font-bold"htmlFor="num4">4</label>
             </li>
-            <li>
-                <input 
-                type="radio"
-                id="num5"
-                name="rating"
-                value='5'
-                onChange={handleChange}
-                checked={selected === 5} />
-                <label htmlFor="num5">5</label>
+            <li className="relative flex items-center justify-center">
+                <input
+                className={inputClassName}
+                    type="radio"
+                    id="num5"
+                    name="rating"
+                    value='5'
+                    onChange={handleChange}
+                    checked={selected === 5} />
+                <label className="absolute mx-auto font-bold"htmlFor="num5">5</label>
             </li>
-            <li>
-                <input 
-                type="radio"
-                id="num6"
-                name="rating"
-                value='6'
-                onChange={handleChange}
-                checked={selected === 6} />
-                <label htmlFor="num6">6</label>
+            <li className="relative flex items-center justify-center">
+                <input
+                className={inputClassName}
+                    type="radio"
+                    id="num6"
+                    name="rating"
+                    value='6'
+                    onChange={handleChange}
+                    checked={selected === 6} />
+                <label className="absolute mx-auto font-bold"htmlFor="num6">6</label>
             </li>
-            <li>
-                <input 
-                type="radio"
-                id="num7"
-                name="rating"
-                value='7'
-                onChange={handleChange}
-                checked={selected === 7} />
-                <label htmlFor="num7">7</label>
+            <li className="relative flex items-center justify-center">
+                <input
+                className={inputClassName}
+                    type="radio"
+                    id="num7"
+                    name="rating"
+                    value='7'
+                    onChange={handleChange}
+                    checked={selected === 7} />
+                <label className="absolute mx-auto font-bold"htmlFor="num7">7</label>
             </li>
-            <li>
-                <input 
-                type="radio"
-                id="num8"
-                name="rating"
-                value='8'
-                onChange={handleChange}
-                checked={selected === 8} />
-                <label htmlFor="num8">8</label>
+            <li className="relative flex items-center justify-center">
+                <input
+                className={inputClassName}
+                    type="radio"
+                    id="num8"
+                    name="rating"
+                    value='8'
+                    onChange={handleChange}
+                    checked={selected === 8} />
+                <label className="absolute mx-auto font-bold"htmlFor="num8">8</label>
             </li>
-            <li>
-                <input 
-                type="radio"
-                id="num9"
-                name="rating"
-                value='9'
-                onChange={handleChange}
-                checked={selected === 9} />
-                <label htmlFor="num9">9</label>
+            <li className="relative flex items-center justify-center">
+                <input
+                className={inputClassName}
+                    type="radio"
+                    id="num9"
+                    name="rating"
+                    value='9'
+                    onChange={handleChange}
+                    checked={selected === 9} />
+                <label className="absolute mx-auto font-bold"htmlFor="num9">9</label>
             </li>
-             <li>
-                <input 
-                type="radio"
-                id="num10"
-                name="rating"
-                value='10'
-                onChange={handleChange}
-                checked={selected === 10} />
-                <label htmlFor="num10">10</label>
+            <li className="relative flex items-center justify-center">
+                <input
+                className={inputClassName}
+                    type="radio"
+                    id="num10"
+                    name="rating"
+                    value='10'
+                    onChange={handleChange}
+                    checked={selected === 10} />
+                <label className="absolute mx-auto font-bold"htmlFor="num10">10</label>
             </li>
         </ul>
     )
