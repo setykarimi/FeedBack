cb974dd2ddc6cdbad62dcf2505af5ac301360fc4
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-01-05 11:53:33 +0330

    Add Button component, set Realtime validation on changing input text

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,10 +1,23 @@
 import { useState } from "react";
+import Button from "./shared/Button";
 import Card from "./shared/Card";
 
 const FeedbackForm = () => {
     const [text, setText] = useState('');
-    
+    const [btnDisabled, setBtnDisabled] = useState(true);
+    const [message, setMessage] = useState('');
+
     const handleTextChange = (e) => {
+        if(text === ''){
+            setBtnDisabled(true)
+            setMessage(null)
+        } else if(text.length !== '' && text.trim().length <= 10){
+            setBtnDisabled(true)
+            setMessage('Text must be at least 10 characters')  
+        } else {
+            setMessage(null)
+            setBtnDisabled(false)
+        }
         setText(e.target.value)
     }
 
@@ -14,9 +27,18 @@ const FeedbackForm = () => {
                 <h2>How would you rate your service with us</h2>
                 {/* @todo - rating select component */}
                 <div className="input-group">
-                    <input type="text" placeholder="Write a review" onChange={handleTextChange} value={text}a/>
-                    <button type="sibmit">Send</button>
+                    <input type="text" placeholder="Write a review" onChange={handleTextChange} value={text} a />
+                    <Button type='submit' isDisabled={btnDisabled}>
+                        Send
+                    </Button>
                 </div>
+                
+                {
+                    message &&
+                    <div className="message">
+                        {message}
+                    </div>
+                }
             </form>
         </Card>
     )
