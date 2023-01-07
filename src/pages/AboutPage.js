98ef6f88f6de89833d640a3a2dbe1edece821697
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-01-07 20:51:16 +0330

    Add link for going to home page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import Card from "../components/shared/Card"
 
 const AboutPage = () => {
@@ -7,7 +8,7 @@ const AboutPage = () => {
             <p>This is a React app to leave feedback for a product or service</p>
             <p>Version: 1.0.0</p>
 
-            <p></p>
+           <Link to='/' className="text-sm font-bold mt-4 block text-orange-600 w-fit hover:underline">Back to home</Link>
         </Card>
     )
 }
