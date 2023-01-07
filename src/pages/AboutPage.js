c70756932415f4db00638c1c621cc9f0f0c4e415
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-01-07 19:59:54 +0330

    Create aboutUs component

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,15 @@
+import Card from "../components/shared/Card"
+
+const AboutPage = () => {
+    return (
+        <Card>
+            <h1>About this Project</h1>
+            <p>This is a React app to leave feedback for a product or service</p>
+            <p>Version: 1.0.0</p>
+
+            <p></p>
+        </Card>
+    )
+}
+
+export default AboutPage
