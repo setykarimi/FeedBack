92ab3191ff5c56477f0ed1c10ab6abd087bd980d
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-01-10 20:09:35 +0330

    Add new const for editedItem and pass it to provider

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -28,20 +28,34 @@ export const FeedbackProvider = ({ children }) => {
     ])
 
     
+    const [feedbackEdit, setFeedbackEdit] = useState({
+      item: {},
+      edit: false
+    })
+
+    
   const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
       setFeedback(feedback.filter((item) => item.id !== id))
     }
   }
 
-
+  // Add feedback
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4()
     setFeedback([...feedback, newFeedback])
   }
 
+  // Set item to be update
+  const editFeedback = (item) => {
+    setFeedback({
+      item,
+      edit: true
+    })
+  }
+
     return (
-    <FeedbackContext.Provider value={{feedback, deleteFeedback, addFeedback}}>
+    <FeedbackContext.Provider value={{feedback, deleteFeedback, addFeedback, editFeedback}}>
         {children}
     </FeedbackContext.Provider>)
 }
