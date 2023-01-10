392442fbfd44afd1acd0d1a68d1850667308f33f
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-01-10 20:58:02 +0330

    Edit icons

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,4 +1,4 @@
-import { FaTimes, FaEdit } from 'react-icons/fa'
+import { TbCircleX, TbEditCircle } from 'react-icons/tb'
 import PropTypes from 'prop-types';
 import Card from "./shared/Card";
 import { useFeedback } from '../context/FeedbackContext';
@@ -12,13 +12,13 @@ const FeedbackItem = ({ item }) => {
             <div className="absolute flex justify-center items-center text-white -top-3 w-8 h-8 bg-orange-500 -left-3 rounded-full">
                 {item.rating}
             </div>
-            <button onClick={() => deleteFeedback(item.id)} className='close absolute right-2 top-2 text-sm'>
-                <FaTimes color='gray'/>
+            <button onClick={() => deleteFeedback(item.id)} className='close absolute right-1 top-1 text-sm'>
+                <TbCircleX color='gray' size="20"/>
             </button>
-            <button onClick={() => editFeedback(item)} className='close absolute right-6 top-2 text-sm'>
-                <FaEdit color='red'/>
+            <button onClick={() => editFeedback(item)} className='close absolute right-6 top-1 text-sm'>
+                <TbEditCircle className='text-blue-500' size="20"/>
             </button>
-            <div className="text-display px-3">
+            <div className="text-display px-3 pt-2">
                 <p>{item.text}</p>
             </div>
         </Card>
